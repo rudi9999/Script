45da970eabe36ff44eb7dffb274bde065f27e3c7
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-07-16 00:35:44 -0300

    Update swapfle.sh

--- swapfle.sh
+++ swapfle.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
-echo ""
-echo "========================"
 echo "creando archivo swap"
 dd if=/dev/zero of=/swapfile bs=1MB count=2048
-echo "========================"
 sleep 3
 echo "comprovando tamaño del archivo"
 ls -hl /swapfile
-echo "========================"
 sleep 3
 echo "asignando permisos"
 chmod 600 /swapfile
-echo "========================"
 sleep 3
 echo "dando formato al achivo"
 mkswap /swapfile
-echo "========================"
 sleep 3
 echo "Activando"
 swapon /swapfile
-echo "========================"
 sleep 3
 echo "activado permanente"
 echo "/swapfile none swap sw 0 0" >> /etc/fstab
-echo "========================"
 sleep 3
 echo "prioridad permanente"
 echo "vm.swappiness=20" >> /etc/sysctl.conf
-echo "========================"
 sleep 3
