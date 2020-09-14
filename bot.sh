977cef266abc86703e5854543e12d8937018974b
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-14 11:16:56 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -58,7 +58,7 @@ while true; do
 	    comando=(${message_text[$id]})
 	    case ${comando[0]} in
 	      /[Tt]este|[Tt]este)teste_fun &;;
-	      /[Ii]d|[Ii]d|/[Ii]D|[Ii]D)teste_fun &;;
+	      /[Ii]d|[Ii]d|/[Ii]D|[Ii]D)myid_fun &;;
 		  /[Aa]juda|[Aa]juda|[Hh]elp|/[Hh]elp)ajuda_fun &;;
 		  /[Ss]tart|[Ss]tart|[Cc]omecar|/[Cc]omecar)ajuda_fun &;;
 		  /[Ll]ogar|[Ll]ogar|[Ll]oguin|/[Ll]oguin)ativarid_fun "${comando[1]}" "${comando[2]}" "$chatuser";;
