2b3a0357d23d74de4959852ec733c3c8a672fab3
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-15 17:30:53 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -40,7 +40,7 @@ ShellBot.init --token "$bot_token"
 ShellBot.username
 
 listID_fun () {
-lsid=$(nl -s "-" ${CID})
+lsid=$(nl -s "." ${CID})
 local bot_retorno="$LINE\n"
           bot_retorno+="Lista de id permitidos\n"
           bot_retorno+="$LINE\n"
