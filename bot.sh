dab20683d7b3e153494ab17896b3b0e952dfaf95
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-15 16:19:53 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -6,6 +6,7 @@ CID="${CIDdir}/Control-ID" && [[ ! -e ${CID} ]] && wget -O ${CID} https://raw.gi
 [[ $(dpkg --get-selections|grep -w "jq"|head -1) ]] || apt-get install jq -y &>/dev/null
 [[ ! -e "/bin/ShellBot.sh" ]] && wget -O /bin/ShellBot.sh https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/ADM-ULTIMATE-NEW-FREE/master/Install/ShellBot.sh &> /dev/null
 [[ -e /etc/texto-bot ]] && rm /etc/texto-bot
+LINE="==========================="
 
 #HORA Y FECHA
 _hora=$(printf '%(%H:%M:%S)T') 
