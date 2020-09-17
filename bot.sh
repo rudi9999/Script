f4d6e963bd0481ab40a2c2cc7fdecfad9f61fa02
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-16 23:13:25 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -39,19 +39,70 @@ bot_token='1249652996:AAE7VsdIppmjKq4O-eX3tk70WdHvPVzz7wA'
 ShellBot.init --token "$bot_token" --monitor --return map
 ShellBot.username
 
-keys_fun () {
-echo "${1}" >> $HOME/prueva.txt
-local bot_retorno="$LINE\n"
-          bot_retorno+="prueva guardada\n"
-          bot_retorno+="$LINE\n"
-	      ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
-							--text "_$(echo -e $bot_retorno)_" \
-							--parse_mode markdown
+sen_fun () {
+
+function download_file()
+{
+	local file_id
+
+	if [[ ${message_photo_file_id[$id]} ]]; then
+		# Em alguns arquivos de imagem o telegram aplica uma escala de tamanho
+		# gerando id's para diferentes resoluções da mesma iagem. São elas (baixa, média, alta).
+		# Os id's são armazenados e separados pelo delimitador '|' (padrão).
+		#
+		# Exemplo:
+		#     baixa_id|media_id|alta_id
+		
+		# Extrai o id da imagem com melhor resolução.
+		file_id=${message_photo_file_id[$id]##*|}
+	else 
+		# Outros objetos.
+		# Extrai o id do objeto da mensagem.
+		# document, audio, sticker, voice
+		file_id=$(cat << _eof
+${message_document_file_id[$id]}
+${message_audio_file_id[$id]}
+${message_sticker_file_id[$id]}
+${message_voice_file_id[$id]}
+_eof
+)
+	fi
+
+	# Verifica se 'file_id' contém um id válido.	
+	if [[ $file_id ]]; then
+		# Obtém informações do arquivo, incluindo sua localização nos servidores do Telegram.
+		ShellBot.getFile --file_id $file_id
+
+		# Baixa o arquivo do diretório remoto contido em '{return[file_path]}' após
+		# a chamada do método 'ShellBot.getFile'.
+		# Obs: Recurso disponível somente no modo de retorno 'map'.
+		if ShellBot.downloadFile --file_path "${return[file_path]}" --dir "$HOME"; then
+			ShellBot.sendMessage	--chat_id "${message_chat_id[$id]}" \
+									--reply_to_message_id "${message_message_id[$id]}" \
+									--text "Arquivo baixado com sucesso!!\n\nSalvo em: ${return[file_path]}"
+		fi
+	fi
+
+	return 0
 }
 
-vie_fun () {
-lsidp=$(cat $HOME/prueva.txt)
-ShellBot.sendMessage --chat_id ${message_chat_id[$id]} --text hola+como+estas!
+while :
+do
+	# Obtem as atualizações
+	ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
+	
+	# Lista o índice das atualizações
+	for id in $(ShellBot.ListUpdates)
+	do
+	# Inicio thread
+	(
+		# Executa a função 'download_file' se a requisição não for um objeto de texto.
+		[[ ${message_text[$id]} ]] || download_file
+
+	) & # Utilize a thread se deseja que o bot responda a várias requisições simultâneas.
+	done
+done
+
 }
 
 listID_fun () {
@@ -249,8 +300,7 @@ while true; do
 	      /[Tt]este)teste_fun &;;
 	      /[Ii]d|/[Ii]D)myid_fun &;;
 	      /[Kk]ey)key_fun &;;
-	      /[Kk]eys)keys_fun "${comando[1]}" &;;
-	      /[Vv]ie)vie_fun &;;
+	      /[Vv]ie)sen_fun &;;
 	      /[Rr]eboot)reboot_fun &;;
 		  /[Aa]yuda|[Aa]yuda|[Hh]elp|/[Hh]elp|/[Ss]tart|[Ss]tart|[Cc]omensar|/[Cc]omensar)ajuda_fun &;;
 		  /[Ii]nfosys)infosys_fun &;;
