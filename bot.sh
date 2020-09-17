16b5befe6746438a187273fbc429453e164c8bf2
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-17 16:20:27 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -150,15 +150,15 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 }
 
 #meu_ip () {
-if [[ -e /etc/MEUIPADM ]]; then
-echo "$(cat /etc/MEUIPADM)"
-else
-MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
-[[ "$MEU_IP" != "$MEU_IP2" ]] && echo "$MEU_IP2" || echo "$MEU_IP"
-echo "$MEU_IP2" > /etc/MEUIPADM
-fi
-}
+#if [[ -e /etc/MEUIPADM ]]; then
+#echo "$(cat /etc/MEUIPADM)"
+#else
+#MEU_IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+#MEU_IP2=$(wget -qO- ipv4.icanhazip.com)
+#[[ "$MEU_IP" != "$MEU_IP2" ]] && echo "$MEU_IP2" || echo "$MEU_IP"
+#echo "$MEU_IP2" > /etc/MEUIPADM
+#fi
+#}
 
 reboot_fun () {
   permited=$(curl -sSL "https://raw.githubusercontent.com/rudi9999/Script/master/Control-Admin")
