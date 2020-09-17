a5e5276f859ab4a009b0d507098e7f47cd547aee
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-17 16:07:59 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -144,6 +144,12 @@ rm $HOME/Control-ID
 }
 
 meu_ip () {
+MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+MIP2=$(wget -qO- ipv4.icanhazip.com)
+[[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
+}
+
+#meu_ip () {
 if [[ -e /etc/MEUIPADM ]]; then
 echo "$(cat /etc/MEUIPADM)"
 else
