134b62c497dc66a60930acd9a312f5e80001912b
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-18 18:25:05 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -63,8 +63,8 @@ number=$(expr length $1)
 for((i=1; i<$number+1; i++)); do
 txt[$i]=$(echo "$1" | cut -b $i)
 case ${txt[$i]} in
-".")txt[$i]="+";;
-"+")txt[$i]=".";;
+".")txt[$i]="*";;
+"*")txt[$i]=".";;
 "1")txt[$i]="@";;
 "@")txt[$i]="1";;
 "2")txt[$i]="?";;
