fd25984ff1a6050e99c7a815e0a499aa83d4c44b
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-19 17:12:14 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -346,7 +346,7 @@ local bot_retorno="$LINE\n"
          bot_retorno+="COMANDOS\n"
 	 bot_retorno+="/ayuda (muestra este menu)\n"
 	 bot_retorno+="/ID (muestra sus ID)\n"
-	 [[ ! $(echo $permited|grep "${chatuser}") = "" ]] && bot_retorno+="/Keygen (requiere permisos)\n"
+	 [[ $(echo $permited|grep "${chatuser}") = "" ]] && bot_retorno+="/Keygen (requiere permisos)\n"
 	 bot_retorno+="$LINE\n"
 	 else
          bot_retorno+="COMANDOS\n"
