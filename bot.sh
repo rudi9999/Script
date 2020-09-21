67bd9c03b7563900f51379d6923f0fb75aedf139
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2020-09-21 02:17:11 -0300

    Update bot.sh

--- bot.sh
+++ bot.sh
@@ -105,7 +105,7 @@ echo "$nombrevalue" > ${DIR}/${KEY}.name
 
 link_fun () {
 local bot_retorno="$LINE\n"
-	bot_retorno+="sudo apt update -y; apt upgrade -y; wget https://raw.githubusercontent.com/rudi9999/VPS-MX-8.0/master/instalscript.sh\n"
+	bot_retorno+="sudo apt update -y; apt upgrade -y; wget https://raw.githubusercontent.com/rudi9999/VPS-MX-8.0/master/instalscript.sh &amp;>&gt;\n"
 	bot_retorno+="$LINE\n"
 	      ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
 							--text "<i>$(echo -e $bot_retorno)</i>" \
