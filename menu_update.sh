ae6b46f13fa843e0b7e2b54b1ec2b4afc6ffdcec
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2021-01-13 20:18:46 -0300

    Update menu_update.sh

--- menu_update.sh
+++ menu_update.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+var=$(pwd)
 cd /etc/newadm
 mv menu menu.back
 wget https://github.com/rudi9999/Script/raw/master/menu
 chmod +x menu
-cd $home
+cd $var
 rm menu_update.sh
 echo " actulizacion completa"
 echo -ne " enter para terminar"
